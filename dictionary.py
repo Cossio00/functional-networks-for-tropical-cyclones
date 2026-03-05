d1d728808ba3c75d6d8f9ad5e505b8ce6927db45
Vinícius Cossio de Oliveira <vinicossio@gmail.com>
2026-03-05 19:33:19 -0300

    added dictionaries to rep

---
+++ dictionary.py
@@ -0,0 +1,32 @@
+CYCLONES = {
+    "Gaja": {
+        "antes": ("2018-10-29 00:00", "2018-11-07 21:00"),
+        "durante": ("2018-11-10 00:00", "2018-11-19 21:00"),
+        "folder": "Gaja",
+        "region": "Bengal_Bay"
+    },
+    "Luban": {
+        "antes": ("2018-09-02 00:00", "2018-09-11 21:00"),
+        "durante": ("2018-10-02 00:00", "2018-10-11 21:00"),
+        "folder": "Luban",
+        "region": "Bengal_Bay"
+    },
+    "Vardah": {
+        "antes": ("2016-11-26 00:00", "2016-12-05 21:00"),
+        "durante": ("2016-12-09 00:00", "2016-12-18 21:00"),
+        "folder": "Vardah",
+        "region": "Bengal_Bay"
+    },
+    "Megh": {
+        "durante": ("2015-11-03 00:00", "2015-11-12 21:00"),
+        "folder": "Megh",
+        "region": "Bengal_Bay"
+    },
+    "Irma": {
+        "durante": ("2017-09-01 00:00", "2017-09-10 21:00"),
+        "folder": "Irma",
+        "region": "North_Atlantic_Ocean"
+    }
+
+}
+
---
+++ dictionary_regions.py
@@ -0,0 +1,15 @@
+REGIONS = {
+    "Bengal_Bay": {
+        "area": [34.5, 49.5, 4.5, 100],
+        "folder": "Bengal_Bay",
+    },
+    "Gulf_of_Mexico": {
+        "area": [31, 262, 18, 280],
+        "folder": "Gulf_of_Mexico",
+    },
+    "North_Atlantic_Ocean": {
+        "area": [42, -100, 10, -42],
+        "folder": "North_Atlantic_Ocean",
+    }
+}
+
