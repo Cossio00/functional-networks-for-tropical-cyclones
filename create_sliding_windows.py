10c9bb6da9f28d85cfa817f566a532170ba42b55
Vinícius Cossio de Oliveira <vinicossio@gmail.com>
2026-03-05 17:52:08 -0300

    time windows updated

--- create_sliding_windows.py
+++ create_sliding_windows.py
@@ -4,14 +4,16 @@ from dictionary import CYCLONES
 def create_sliding_windows(region, cyclone):
 
     print("Abrindo arquivo de anomalias com máscara oceânica...")
-    anoms_before = xr.open_dataarray(f"Metrics/{region}/{cyclone}/{cyclone}_ocean_anomalies_before.nc")
+    if "antes" in CYCLONES[cyclone]:
+        anoms_before = xr.open_dataarray(f"Metrics/{region}/{cyclone}/{cyclone}_ocean_anomalies_before.nc")
     anoms_during = xr.open_dataarray(f"Metrics/{region}/{cyclone}/{cyclone}_ocean_anomalies_during.nc")
 
-
-    window_before = anoms_before.sel(time=slice(CYCLONES[cyclone]["antes"][0], CYCLONES[cyclone]["antes"][1]))
+    if "antes" in CYCLONES[cyclone]:
+        window_before = anoms_before.sel(time=slice(CYCLONES[cyclone]["antes"][0], CYCLONES[cyclone]["antes"][1]))
+        print(f"Janela antes:   {len(window_before.time)} timesteps")
+    
     window_during = anoms_during.sel(time=slice(CYCLONES[cyclone]["durante"][0], CYCLONES[cyclone]["durante"][1]))
 
-    print(f"Janela antes:   {len(window_before.time)} timesteps")
     print(f"Janela durante: {len(window_during.time)} timesteps")
 
     # ===================================================================
@@ -25,11 +27,13 @@ def create_sliding_windows(region, cyclone):
         }
     }
 
-
-    window_before.to_netcdf(
-        f"Metrics/{region}/{cyclone}/windows_before_{cyclone}.nc",
-        engine='netcdf4'  
-    )
+    if "antes" in CYCLONES[cyclone]:
+        window_before.to_netcdf(
+            f"Metrics/{region}/{cyclone}/windows_before_{cyclone}.nc",
+            engine='netcdf4'  
+        )
+    else:
+        print(f"\nCiclone {cyclone} não possui período 'antes'. Pulando...")
 
     window_during.to_netcdf(
         f"Metrics/{region}/{cyclone}/windows_during_{cyclone}.nc",
