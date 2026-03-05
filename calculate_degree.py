62e594df3c8016e60163448ddf2847aab0a6882f
Vinícius Cossio de Oliveira <vinicossio@gmail.com>
2026-03-05 18:03:06 -0300

    updated degree calculation

--- calculate_degree.py
+++ calculate_degree.py
@@ -1,34 +1,44 @@
 import pickle
 import numpy as np
 import networkx as nx
+from dictionary import CYCLONES
 
 
 def calculate_degree(region, cyclone):
 
+    print(pickle.__version__)
     # ------------------------------------------------------------------
     # 1. Carregar o pickle gerado anteriormente no cálculo de tau de Kendall
     # ------------------------------------------------------------------
     with open(f"Metrics/{region}/{cyclone}/{cyclone}_metrics.pkl", "rb") as f:
         data = pickle.load(f)
-
-    antes   = data['antes']
+    
+    if "antes" in CYCLONES[cyclone]:
+        antes   = data['antes']
     durante = data['durante']
 
     # ------------------------------------------------------------------
     # 2. Criar os grafos a partir das matrizes de adjacência
     # ------------------------------------------------------------------
-    G_antes   = nx.from_numpy_array(antes['adjacency_matrix'])
+    if "antes" in CYCLONES[cyclone]:
+        G_antes   = nx.from_numpy_array(antes['adjacency_matrix'])
     G_durante = nx.from_numpy_array(durante['adjacency_matrix'])
 
     # ------------------------------------------------------------------
     # 3. Calcular o degree 
     # ------------------------------------------------------------------
-    degree_antes   = np.array([G_antes.degree(i)   for i in range(G_antes.number_of_nodes())])
+    if "antes" in CYCLONES[cyclone]:
+        degree_antes   = np.array([G_antes.degree(i)   for i in range(G_antes.number_of_nodes())])
     degree_durante = np.array([G_durante.degree(i) for i in range(G_durante.number_of_nodes())])
 
-
-    antes['degree']   = degree_antes
+    if "antes" in CYCLONES[cyclone]:
+        antes['degree']   = degree_antes
     durante['degree'] = degree_durante
 
-    with open(f"Metrics/{region}/{cyclone}/{cyclone}_metrics.pkl", "wb") as f:
-        pickle.dump({'antes': antes, 'durante': durante}, f)
+    if "antes" in CYCLONES[cyclone]:
+        with open(f"Metrics/{region}/{cyclone}/{cyclone}_metrics.pkl", "wb") as f:
+            pickle.dump({'antes': antes, 'durante': durante}, f)
+    else:
+        with open(f"Metrics/{region}/{cyclone}/{cyclone}_metrics.pkl", "wb") as f:
+            pickle.dump({'durante': durante}, f)
+    
